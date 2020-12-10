7fbdfc0f09cbb060217f339df1bd7c67ed24b531
ilovepilav <cagrisakaoglu@gmail.com>
2020-12-10 18:22:28 +0300

    Created simple windows form UI for access,execute and retrieve query examples

--- DbFirst/Form1.Designer.cs
+++ DbFirst/Form1.Designer.cs
@@ -29,13 +29,233 @@ namespace DbFirst
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.gbResults = new System.Windows.Forms.GroupBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.gbQueryDetails = new System.Windows.Forms.GroupBox();
+            this.gbQueries = new System.Windows.Forms.GroupBox();
+            this.btnQuery1 = new System.Windows.Forms.Button();
+            this.btnQuery2 = new System.Windows.Forms.Button();
+            this.btnQuery3 = new System.Windows.Forms.Button();
+            this.btnQuery4 = new System.Windows.Forms.Button();
+            this.btnQuery5 = new System.Windows.Forms.Button();
+            this.btnQuery6 = new System.Windows.Forms.Button();
+            this.btnQuery7 = new System.Windows.Forms.Button();
+            this.btnQuery8 = new System.Windows.Forms.Button();
+            this.btnQuery9 = new System.Windows.Forms.Button();
+            this.btnQuery10 = new System.Windows.Forms.Button();
+            this.btnQuery11 = new System.Windows.Forms.Button();
+            this.btnQuery12 = new System.Windows.Forms.Button();
+            this.lblQueryDetail = new System.Windows.Forms.Label();
+            this.gbResults.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.gbQueryDetails.SuspendLayout();
+            this.gbQueries.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // gbResults
+            // 
+            this.gbResults.Controls.Add(this.dataGridView1);
+            this.gbResults.Location = new System.Drawing.Point(13, 14);
+            this.gbResults.Name = "gbResults";
+            this.gbResults.Size = new System.Drawing.Size(746, 267);
+            this.gbResults.TabIndex = 0;
+            this.gbResults.TabStop = false;
+            this.gbResults.Text = "Results";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(7, 21);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(733, 239);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // gbQueryDetails
+            // 
+            this.gbQueryDetails.Controls.Add(this.lblQueryDetail);
+            this.gbQueryDetails.Location = new System.Drawing.Point(20, 288);
+            this.gbQueryDetails.Name = "gbQueryDetails";
+            this.gbQueryDetails.Size = new System.Drawing.Size(328, 101);
+            this.gbQueryDetails.TabIndex = 1;
+            this.gbQueryDetails.TabStop = false;
+            this.gbQueryDetails.Text = "Query Details";
+            // 
+            // gbQueries
+            // 
+            this.gbQueries.Controls.Add(this.btnQuery12);
+            this.gbQueries.Controls.Add(this.btnQuery11);
+            this.gbQueries.Controls.Add(this.btnQuery6);
+            this.gbQueries.Controls.Add(this.btnQuery10);
+            this.gbQueries.Controls.Add(this.btnQuery5);
+            this.gbQueries.Controls.Add(this.btnQuery9);
+            this.gbQueries.Controls.Add(this.btnQuery4);
+            this.gbQueries.Controls.Add(this.btnQuery8);
+            this.gbQueries.Controls.Add(this.btnQuery3);
+            this.gbQueries.Controls.Add(this.btnQuery7);
+            this.gbQueries.Controls.Add(this.btnQuery2);
+            this.gbQueries.Controls.Add(this.btnQuery1);
+            this.gbQueries.Location = new System.Drawing.Point(354, 288);
+            this.gbQueries.Name = "gbQueries";
+            this.gbQueries.Size = new System.Drawing.Size(405, 101);
+            this.gbQueries.TabIndex = 2;
+            this.gbQueries.TabStop = false;
+            this.gbQueries.Text = "Queries!";
+            // 
+            // btnQuery1
+            // 
+            this.btnQuery1.Location = new System.Drawing.Point(6, 26);
+            this.btnQuery1.Name = "btnQuery1";
+            this.btnQuery1.Size = new System.Drawing.Size(59, 23);
+            this.btnQuery1.TabIndex = 0;
+            this.btnQuery1.Text = "Query1";
+            this.btnQuery1.UseVisualStyleBackColor = true;
+            // 
+            // btnQuery2
+            // 
+            this.btnQuery2.Location = new System.Drawing.Point(71, 26);
+            this.btnQuery2.Name = "btnQuery2";
+            this.btnQuery2.Size = new System.Drawing.Size(59, 23);
+            this.btnQuery2.TabIndex = 0;
+            this.btnQuery2.Text = "Query2";
+            this.btnQuery2.UseVisualStyleBackColor = true;
+            // 
+            // btnQuery3
+            // 
+            this.btnQuery3.Location = new System.Drawing.Point(136, 26);
+            this.btnQuery3.Name = "btnQuery3";
+            this.btnQuery3.Size = new System.Drawing.Size(59, 23);
+            this.btnQuery3.TabIndex = 0;
+            this.btnQuery3.Text = "Query3";
+            this.btnQuery3.UseVisualStyleBackColor = true;
+            // 
+            // btnQuery4
+            // 
+            this.btnQuery4.Location = new System.Drawing.Point(201, 26);
+            this.btnQuery4.Name = "btnQuery4";
+            this.btnQuery4.Size = new System.Drawing.Size(59, 23);
+            this.btnQuery4.TabIndex = 0;
+            this.btnQuery4.Text = "Query4";
+            this.btnQuery4.UseVisualStyleBackColor = true;
+            // 
+            // btnQuery5
+            // 
+            this.btnQuery5.Location = new System.Drawing.Point(266, 26);
+            this.btnQuery5.Name = "btnQuery5";
+            this.btnQuery5.Size = new System.Drawing.Size(59, 23);
+            this.btnQuery5.TabIndex = 0;
+            this.btnQuery5.Text = "Query5";
+            this.btnQuery5.UseVisualStyleBackColor = true;
+            // 
+            // btnQuery6
+            // 
+            this.btnQuery6.Location = new System.Drawing.Point(331, 26);
+            this.btnQuery6.Name = "btnQuery6";
+            this.btnQuery6.Size = new System.Drawing.Size(59, 23);
+            this.btnQuery6.TabIndex = 0;
+            this.btnQuery6.Text = "Query6";
+            this.btnQuery6.UseVisualStyleBackColor = true;
+            // 
+            // btnQuery7
+            // 
+            this.btnQuery7.Location = new System.Drawing.Point(6, 55);
+            this.btnQuery7.Name = "btnQuery7";
+            this.btnQuery7.Size = new System.Drawing.Size(59, 23);
+            this.btnQuery7.TabIndex = 0;
+            this.btnQuery7.Text = "Query7";
+            this.btnQuery7.UseVisualStyleBackColor = true;
+            // 
+            // btnQuery8
+            // 
+            this.btnQuery8.Location = new System.Drawing.Point(71, 55);
+            this.btnQuery8.Name = "btnQuery8";
+            this.btnQuery8.Size = new System.Drawing.Size(59, 23);
+            this.btnQuery8.TabIndex = 0;
+            this.btnQuery8.Text = "Query8";
+            this.btnQuery8.UseVisualStyleBackColor = true;
+            // 
+            // btnQuery9
+            // 
+            this.btnQuery9.Location = new System.Drawing.Point(136, 55);
+            this.btnQuery9.Name = "btnQuery9";
+            this.btnQuery9.Size = new System.Drawing.Size(59, 23);
+            this.btnQuery9.TabIndex = 0;
+            this.btnQuery9.Text = "Query9";
+            this.btnQuery9.UseVisualStyleBackColor = true;
+            // 
+            // btnQuery10
+            // 
+            this.btnQuery10.Location = new System.Drawing.Point(201, 55);
+            this.btnQuery10.Name = "btnQuery10";
+            this.btnQuery10.Size = new System.Drawing.Size(59, 23);
+            this.btnQuery10.TabIndex = 0;
+            this.btnQuery10.Text = "Query10";
+            this.btnQuery10.UseVisualStyleBackColor = true;
+            // 
+            // btnQuery11
+            // 
+            this.btnQuery11.Location = new System.Drawing.Point(266, 55);
+            this.btnQuery11.Name = "btnQuery11";
+            this.btnQuery11.Size = new System.Drawing.Size(59, 23);
+            this.btnQuery11.TabIndex = 0;
+            this.btnQuery11.Text = "Query11";
+            this.btnQuery11.UseVisualStyleBackColor = true;
+            // 
+            // btnQuery12
+            // 
+            this.btnQuery12.Location = new System.Drawing.Point(331, 55);
+            this.btnQuery12.Name = "btnQuery12";
+            this.btnQuery12.Size = new System.Drawing.Size(59, 23);
+            this.btnQuery12.TabIndex = 0;
+            this.btnQuery12.Text = "Query12";
+            this.btnQuery12.UseVisualStyleBackColor = true;
+            // 
+            // lblQueryDetail
+            // 
+            this.lblQueryDetail.AutoSize = true;
+            this.lblQueryDetail.Location = new System.Drawing.Point(7, 22);
+            this.lblQueryDetail.Name = "lblQueryDetail";
+            this.lblQueryDetail.Size = new System.Drawing.Size(0, 14);
+            this.lblQueryDetail.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 14F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(769, 401);
+            this.Controls.Add(this.gbQueries);
+            this.Controls.Add(this.gbQueryDetails);
+            this.Controls.Add(this.gbResults);
+            this.Font = new System.Drawing.Font("Calibri", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.gbResults.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.gbQueryDetails.ResumeLayout(false);
+            this.gbQueryDetails.PerformLayout();
+            this.gbQueries.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox gbResults;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.GroupBox gbQueryDetails;
+        private System.Windows.Forms.Label lblQueryDetail;
+        private System.Windows.Forms.GroupBox gbQueries;
+        private System.Windows.Forms.Button btnQuery12;
+        private System.Windows.Forms.Button btnQuery11;
+        private System.Windows.Forms.Button btnQuery6;
+        private System.Windows.Forms.Button btnQuery10;
+        private System.Windows.Forms.Button btnQuery5;
+        private System.Windows.Forms.Button btnQuery9;
+        private System.Windows.Forms.Button btnQuery4;
+        private System.Windows.Forms.Button btnQuery8;
+        private System.Windows.Forms.Button btnQuery3;
+        private System.Windows.Forms.Button btnQuery7;
+        private System.Windows.Forms.Button btnQuery2;
+        private System.Windows.Forms.Button btnQuery1;
     }
 }
 
